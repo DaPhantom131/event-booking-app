5c2b2fef69c2eec2d2bfe670eb6ba73c9cb74d23
DaPhantom131 <danielokunor8@gmail.com>
2026-06-18 15:35:07 +0100

    I building the navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import { BrowserRouter,Routes,Route } from "react-router-dom";
 import Home from "./Components/Routes/Home";
 import Navbar from "./Components/Navbar";
 import Events from "./Components/Routes/Events";
+import Booking from "./Components/Routes/Booking";
+
+
+import "./Styles/Main.css";
 
 function App() {
 
@@ -14,9 +18,13 @@ function App() {
         <Navbar/>
 
         <Routes>
+
           <Route path="/" element={<Home/>}/>
 
           <Route path="/events" element={<Events/>}/>
+
+          <Route path="/bookings" element={<Booking/>}/>
+
         </Routes>
 
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../Styles/Navbar.css";
 
 const Navbar = () =>{
 return(
     <React.Fragment>
-        <h3>This is the Navigation bar</h3>
+        <nav className="header">
 
-     <Link to={"/"}>Home</Link>
+            <div className="nav-logo">My Logo</div>
 
-     <Link to={"/events"}>Events</Link>
-     
+                <div className="nav-links">
+
+                    <Link to={"/"} className="links">Home</Link>
+
+                    <Link to={"/events"} className="links">Events</Link>
+
+                    <Link to={"/bookings"} className="links">Bookings</Link>
+
+                </div>
+
+        </nav>
+
     </React.Fragment>
 )
 
