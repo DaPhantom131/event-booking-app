305911d26d9c8527222ebf942adb661abf3e5c07
DaPhantom131 <danielokunor8@gmail.com>
2026-06-25 18:19:34 +0100

    added the search filtering
    Implemented search functionality on the Events page using React state management. Developed a search input field and filtered event data dynamically based on user input using the JavaScript filter() method. Tested the functionality to ensure only matching events were displayed.

--- src/Components/Routes/Events.jsx
+++ src/Components/Routes/Events.jsx
@@ -4,15 +4,38 @@ import EventCard from "../EventCard";
 import {events} from "../../Data/event.js"
 import "../../Styles/EventCard.css";
 
+import { useState } from "react";
+
 const Events = () =>{
+
+    const [searchTerm, setSearchTerm] = useState(events);
+
+    function onChange(e) {
+        const value = e.target.value.toLowerCase();
+
+        const Filtered = events.filter(event =>
+            event.title.toLowerCase().includes(value)
+        )
+
+        setSearchTerm(Filtered)
+    }
+
+
     return(
         <div className="event-page">
 
             <h1>Upcoming Events</h1>
 
+            <div className="search-bar">
+                <input
+                 type="text" 
+                 placeholder="Search"
+                onChange={onChange} />
+            </div>
+
             <div className="all-event-cards">
 
-                {events.map((event) => (
+                {searchTerm.map((event) => (
                     <EventCard
                         key={event.id}
                         id={event.id}
